b24e179b95b0c1188d0e102bf38e66522c27da96
NoodlesPi <58204020+NoodlesPi@users.noreply.github.com>
2019-12-24 00:34:15 +0800

    Final presentation file modify

--- test.py
+++ test.py
@@ -23,18 +23,17 @@ from ryu.lib.packet import packet, ether_types
 from ryu.lib.packet import ethernet, arp, icmp
 
 from ofctl_utils import OfCtl, VLANID_NONE
-
+import time
 from topo_manager_example import TopoManager
 
 import numpy as np
 
 class ShortestPathSwitching(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
-
+    flag = True
 
 
     def update(self):
-
         self.switch_to = [[1000000 for i in range(self.switchNum+1)] for i in range(self.switchNum+1)]
 
         for x in range(1, self.switchNum+1):
@@ -51,6 +50,8 @@ class ShortestPathSwitching(app_manager.RyuApp):
                 for j in range(1, self.switchNum+1):
                     if self.switch_to[i][k] + self.switch_to[k][j] < self.switch_to[i][j]:
                         self.switch_to[i][j] = self.switch_to[i][k] + self.switch_to[k][j]
+        self.set_flowtable()
+        self.print_path()
 
 
     def __init__(self, *args, **kwargs):
@@ -77,7 +78,7 @@ class ShortestPathSwitching(app_manager.RyuApp):
         self.mac_mac = {}
 
         self.sMac_num = {}
-        self.hIp_num = {}
+        self.hIp_realNum = {}
 
         self.sId_sWitch = {}  # 交换机id和交换机datapath对应
 
@@ -93,7 +94,7 @@ class ShortestPathSwitching(app_manager.RyuApp):
     @set_ev_cls(event.EventSwitchEnter)
     def handle_switch_add(self, ev):
         """
-        Event handler indicating a switch has come online.
+        Event handler indicating a switch has come onlin            self.logger.info(paths)e.
         """
         switch = ev.switch
         dp = switch.dp
@@ -131,12 +132,13 @@ class ShortestPathSwitching(app_manager.RyuApp):
         for port in switch.ports:
             self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
 
-        realNum = self.switch_num_real[switch.dp.id]
-        self.leaveSwitchList.append(realNum)
+        fakeNum = self.switch_real_num[switch.dp.id]
+        self.leaveSwitchList.append(fakeNum)
 
-        for mac in self.numInfo:
-            del self.mac_mac[self.mac_mac[mac]]
-            del self.mac_mac[mac]
+        for mac in self.numInfo[fakeNum]:  ####################################################
+            if mac in self.mac_mac:
+                del self.mac_mac[self.mac_mac[mac]]
+                del self.mac_mac[mac]
 
         # TODO:  Update network topology and flow rules
         self.update()
@@ -158,8 +160,9 @@ class ShortestPathSwitching(app_manager.RyuApp):
         self.hIp_hMac[host.ipv4[0]] = host.mac
         self.hMac_sMac[host.mac] = host.port.hw_addr
 
-        realNum = self.switch_num_real[host.port.dpid]
-        self.hIp_num=realNum
+        # real = self.switch_real_num[host.port.dpid] ########################################
+        self.hIp_realNum[host.ipv4[0]]= host.port.dpid #####################
+
 
         self.tm.add_host(host)
         self.update()
@@ -199,8 +202,12 @@ class ShortestPathSwitching(app_manager.RyuApp):
                           dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
         #########################################################################################################
 
-        del self.mac_mac[src_port.hw_addr]
-        del self.mac_mac[dst_port.hw_addr]
+        if src_port.hw_addr in self.mac_mac:
+
+            del self.mac_mac[src_port.hw_addr]
+        if dst_port.hw_addr in self.mac_mac:
+
+            del self.mac_mac[dst_port.hw_addr]
 
         #########################################################################################################
         # TODO:  Update network topology and flow rules
@@ -217,17 +224,31 @@ class ShortestPathSwitching(app_manager.RyuApp):
                          port.dpid, port.port_no, port.hw_addr,
                          "UP" if port.is_live() else "DOWN")
         #########################################################################################################
+        a = self.sMac_num[port.hw_addr]
 
-        if port.hw_addr in self.DelPortList:
-            self.DelPortList.remove(port.hw_addr)
-        else:
-            self.DelPortList.append(port.hw_addr)
-            if port.hw_addr in self.mac_mac:
-                del self.mac_mac[self.mac_mac[port.hw_addr]]
-                del self.mac_mac[port.hw_addr]
+        if port.hw_addr in self.mac_mac:
+            b = self.sMac_num[self.mac_mac[port.hw_addr]]
+
+            if port.hw_addr in self.DelPortList:
+                self.DelPortList.remove(port.hw_addr)
+                self.switch_to[a][b] = 1
+                self.switch_to[b][a] = 1
+            else:
+                self.DelPortList.append(port.hw_addr)
+                if port.hw_addr in self.mac_mac:
+                    # del self.mac_mac[self.mac_mac[port.hw_addr]]
+                    # del self.mac_mac[port.hw_addr]
+                    self.switch_to[a][b] = 1000000
+                    self.switch_to[b][a] = 1000000
         #########################################################################################################
         # TODO:  Update network topology and flow rules
         self.update()
+
+    def delete_forwarding_rule(self, datapath, dl_dst):
+        ofctl = OfCtl.factory(datapath, self.logger)
+
+        match = datapath.ofproto_parser.OFPMatch(dl_dst=dl_dst)
+        ofctl.delete_flow(cookie=0, priority=0, match=match)
 
     def add_forwarding_rule(self, datapath, dl_dst, port):
         ofctl = OfCtl.factory(datapath, self.logger)
@@ -242,6 +263,7 @@ class ShortestPathSwitching(app_manager.RyuApp):
         """
        EventHandler for PacketIn messages
         """
+
         msg = ev.msg
 
         # In OpenFlow, switches are called "datapaths".  Each switch gets its own datapath ID.
@@ -259,18 +281,11 @@ class ShortestPathSwitching(app_manager.RyuApp):
             arp_msg = pkt.get_protocols(arp.arp)[0]
 
             if arp_msg.opcode == arp.ARP_REQUEST:
-
                 self.logger.warning("Received ARP REQUEST on switch%d/%d:  Who has %s?  Tell %s",
                                     dp.id, in_port, arp_msg.dst_ip, arp_msg.src_mac)
-
+                paths = self.get_path(arp_msg.src_ip, arp_msg.dst_ip)
+                self.logger.info('Path:%s',paths)
                 # TODO:  Generate a *REPLY* for this request based on your switch state
-                mac = self.hIp_hMac[arp_msg.dst_ip]
-                port = self.sMac_sPort[self.hMac_sMac[mac]]
-                paths = self.get_path(arp_msg.src_ip,arp_msg.dst_ip)
-                self.logger.info(paths)
-                for tuple in paths:
-                    self.add_forwarding_rule(self.sId_sWitch[tuple[0]], mac, tuple[1])
-
                 ofctl.send_arp(
                     vlan_id=VLANID_NONE,
                     src_port=ofctl.dp.ofproto.OFPP_CONTROLLER,
@@ -291,6 +306,18 @@ class ShortestPathSwitching(app_manager.RyuApp):
 
 
 
+    def print_path(self):
+        host_table = self.get_hostTable()
+        ad_table = self.get_linkTable()
+        for index in range(len(ad_table)):
+            if index != 0:
+                string = 'Switch' + str(index) + ':'
+                for tuple in ad_table[index]:
+                    string += 'port' + str(tuple[1]) + '->Switch' + str(tuple[0]) + ' | '
+                for host in host_table:
+                    if host[1] == index:
+                        string += 'port' + str(host[2]) + '->Host-' + str(host[0]) + ' | '
+                self.logger.info(string)
 
     def get_path(self,src_ip,dst_ip):
         ipA = src_ip  #输入其实主机IP
@@ -322,11 +349,59 @@ class ShortestPathSwitching(app_manager.RyuApp):
                         outputPort = self.sMac_sPort[mac]
 
             ans_list.append((outputNum, outputPort))
-
+            testNum = nowNum
             nowNum = nextNum
+            if testNum == nowNum:
+                return []
+
 
         lastNum = self.switch_num_real[endNum]
         lastPort = self.sMac_sPort[endMac]
         ans_list.append((lastNum,lastPort))
         return ans_list
 
+    def set_flowtable(self):
+        ipList = [] #所有ip
+        for key in self.hIp_hMac:
+            ipList.append(key)
+        #任意两个ip之间都添加对应的流表
+        for ip1 in ipList:
+            for ip2 in ipList:
+                if ip1 != ip2:
+                    tuple_list = self.get_path(ip1,ip2)
+                    for tuple in tuple_list:
+                        #先删除旧流表再添加流表
+                        self.delete_forwarding_rule(self.sId_sWitch[tuple[0]], self.hIp_hMac[ip2])
+                        self.add_forwarding_rule(self.sId_sWitch[tuple[0]], self.hIp_hMac[ip2], tuple[1])
+
+    def get_linkTable(self):
+
+        table = []
+        for i in range(self.switchNum+1):
+            table.append([])
+
+        for link in self.numInfo:
+            start = 0
+            for mac in link:
+                if mac in self.mac_mac:
+                    start = self.switch_num_real[self.sMac_num[mac]]
+                    aim = self.switch_num_real[self.sMac_num[self.mac_mac[mac]]]
+                    port = self.sMac_sPort[mac]
+                    table[start].append((aim,port))
+        return table
+
+    def get_hostTable(self):
+        table = []
+
+
+
+        for i in self.hIp_realNum:
+            num = self.hIp_realNum[i]
+            mac = self.hIp_hMac[i]
+            aimMac = self.hMac_sMac[mac]
+            aimFake = self.sMac_num[aimMac]
+            aim = self.switch_num_real[aimFake]
+
+            port = self.sMac_sPort[aimMac]
+            table.append((i,aim,port))
+        return table
